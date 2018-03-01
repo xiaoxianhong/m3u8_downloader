0079dd811e6d0aeac048d9b08d77886dc3260ebe
xiaoxianhong <542471314@qq.com>
2018-03-01 10:05:27 +0800

    ver0.3 完成所有爬取功能，未完成trycatch和搜索

--- PycharmProjects/untitled1/m3u8/download_movie.py
+++ PycharmProjects/untitled1/m3u8/download_movie.py
@@ -9,7 +9,7 @@ def download(dict,path):
     #设定文件保存地址
     save_path = os.path.dirname(os.path.abspath(__file__))+ path +os.path.basename(dict['Link'])
     print(os.path.abspath(save_path))
-
+    #获取上载地址
     url = dict['Link']
     #获取文件大小，不取文件，直接获取头
     total_length = int(requests.head(url).headers['Content-Length'])
@@ -32,6 +32,7 @@ def download(dict,path):
             i += 1
             print("进度:%s/%s"%(i,N))
     f.close()
+    return
 
 
 
@@ -43,10 +44,36 @@ def m3u8(dict):
     data = data.text.splitlines()[-1]
     print(data)
     datam3u8 = requests.get("http://zuikzy.wb699.com"+data)
-    print(datam3u8)
-    # dict['Link'] = r'http://zuikzy.wb699.com'+data
-    # strpath = '/downlaod/m3u8/' #+ dict['Name'] +'/'
-    # download(dict,strpath)
+    ospath = os.path.dirname(os.path.abspath(__file__))+'/downlaod/m3u8/' + dict['Name']
+    #筛选内容，生成字典，供下载使用
+    m3u8List = []
+    for line in str(datam3u8.text).splitlines():
+        if '.ts' in line:
+            m3u8List.append({'Name':dict['Name'],'Link':"http://zuikzy.wb699.com"+line})
+        else:
+            continue
+    print(m3u8List)
+
+    strpath = '/download/m3u8/' #+ dict['Name'] +'/'
+    # 按字典分件下载
+    for item in m3u8List:
+        download(item,strpath)
+    print('下载完成，开始合并')
+    #使用shell的cat 1.ts 2.ts > combine.ts语句合并ts
+    #os.system
+    tmp =[]
+    for item in m3u8List:
+        tmp.append(os.path.basename(item['Link']))
+    # 合并ts文件
+    os.chdir(os.path.dirname(__file__)+'/download/m3u8/')
+    shell_str = ' '.join(tmp)
+    shell_str = 'cat '+ shell_str + '>1' + dict['Name']+'.ddt'
+    print(shell_str)
+    os.system(shell_str)
+    # 删除ts和m3u8文件
+    os.system('del /Q *.ts')
+    #重命名ddt为ts
+    os.renames(dict['Name']+'.ddt',dict['Name']+'.ts')
 
 def other(dict):
     strpath = '/download/other/'
